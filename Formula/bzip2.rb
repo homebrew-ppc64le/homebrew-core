3d229ed497a4ee1ab0880ce1ef4d675f0da5e8eb
Trung Lê <trung.le@ruby-journal.com>
2020-05-29 01:12:25 +1000

    bzip2: add ppc64le linux binary link

--- Formula/bzip2.rb
+++ Formula/bzip2.rb
@@ -11,6 +11,7 @@ class Bzip2 < Formula
     sha256 "c7f2266c2d354c706de5163c23bb7b7204f1f15a85027ea486877a0c5d253336" => :high_sierra
     sha256 "1f11350ccb9a3bd1dd250b5e440d68a5ea65408d4b91f9eae2aa7628e899b7c5" => :sierra
     sha256 "e5fce257b9fee119d28d0e7f7dac9332bd26b248fe35d983ba85104243e4bc2e" => :x86_64_linux
+    sha256 "a5a2fe845637e543ede296e098c282484c5cd6624a13e83ab6649432f3be2643" => :ppc64le_linux
   end
 
   keg_only :provided_by_macos
