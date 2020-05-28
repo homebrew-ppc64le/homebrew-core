a5e7d3b3ccc6bb4fab22b682dfb6cadb139d605c
Trung Lê <trung.le@ruby-journal.com>
2020-05-28 21:21:00 +1000

    gdbm: Add ppc64le linux binary link

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -12,6 +12,7 @@ class Gdbm < Formula
     sha256 "ac688d571f9c00e09670440d67d2869a34dab0fb897ba0b183ed84fceffdbc9c" => :high_sierra
     sha256 "89d6db4fbffbe2184b4531faaebf0432a4b01e1ed92678ce6bd2f95c69dc9803" => :sierra
     sha256 "80fc4bdfef26e3df4c1f7390e1169d32935faa69f00888318bf0ed8321798aad" => :x86_64_linux
+    sha256 "f5d2fa3a937b36543bcdff0124bcaf4505c91c85f3fa2907710e20b4c451ae93" => :ppc64le_linux
   end
 
   # Use --without-readline because readline detection is broken in 1.13
