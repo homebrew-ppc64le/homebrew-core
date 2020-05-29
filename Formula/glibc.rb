bbece6dd6ca5a4018ec71131a53766862c9ba54d
Trung Lê <trung.le@ruby-journal.com>
2020-05-29 16:11:50 +1000

    glibc: update ppc64le linux link

--- Formula/glibc.rb
+++ Formula/glibc.rb
@@ -62,7 +62,7 @@ class Glibc < Formula
 
   bottle do
     sha256 "654794e9e18c2401f1101a3fcf0a85eda448b4b969e9a99782a3f4f4659feda4" => :x86_64_linux
-    sha256 "5b728569aaf08883eac8d1671caf9b8863bb6bc9fc54a3d3b2290b40d466b3d7" => :ppc64le_linux
+    sha256 "af8533f0de3cca6cdcbef537634a6f1828179ca195aaaa1d2ac16878a7cc482f" => :ppc64le_linux
   end
 
   on_linux do
